338678b2b04ef33a228f22566468483fc66c070d
Robert Rosa <r.j.r.eol@gmail.com>
2017-03-16 11:34:44 -0400

    Added Theme
    -TODO Remove Garbage

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,9 +18,21 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/pace-theme-flash.min.css',
+        'css/animate.min.css',
+        'css/jquery.scrollbar.css',
+        'css/webarch.css'
     ];
     public $js = [
+        'js/pace.min.js',
+        'js/bootstrap.min.js',
+        'js/jqueryblockui.min.js',
+        'js/jquery.unveil.min.js',
+        'js/jquery.scrollbar.min.js',
+        'js/jquery.animateNumbers.min.js',
+        'js/jquery.validate.min.js',
+        'js/select2.min.js',
+        'js/webarch.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
