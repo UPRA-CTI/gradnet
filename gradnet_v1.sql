7734f9ca967a006d50c0789a444afc261ff841e3
Erick Rivera <erick.rivera6@upr.edu>
2017-03-17 13:06:40 -0400

    Db and configuration

--- gradnet_v1.sql
+++ gradnet_v1.sql
@@ -1,22 +1,27 @@
-/*
-SQLyog Community v12.3.1 (64 bit)
-MySQL - 10.1.16-MariaDB : Database - gradnet_v1
-*********************************************************************
-*/
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.1.16-MariaDB)
+# Database: gradnet_v1
+# Generation Time: 2017-03-17 16:54:59 +0000
+# ************************************************************
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`gradnet_v1` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `gradnet_v1`;
 
-/*Table structure for table `grad_addresses` */
+# Dump of table grad_addresses
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `grad_addresses`;
 
@@ -35,20 +40,30 @@ CREATE TABLE `grad_addresses` (
   CONSTRAINT `grad_addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `grad_addresses` */
 
-/*Table structure for table `grad_contact_details` */
+
+# Dump of table grad_contact_details
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `grad_contact_details`;
 
 CREATE TABLE `grad_contact_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
+  `user_id` int(10) DEFAULT NULL,
+  `phone_number1` int(10) DEFAULT NULL,
+  `phone_number2` int(11) DEFAULT NULL,
+  `phone_type` varchar(128) DEFAULT NULL,
+  `created_at` int(10) DEFAULT NULL,
+  `updated_at` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`user_id`),
+  CONSTRAINT `id` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `grad_contact_details` */
 
-/*Table structure for table `grad_events_stream` */
+
+# Dump of table grad_events_stream
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `grad_events_stream`;
 
@@ -62,9 +77,10 @@ CREATE TABLE `grad_events_stream` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `grad_events_stream` */
 
-/*Table structure for table `grad_mayor` */
+
+# Dump of table grad_mayor
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `grad_mayor`;
 
@@ -82,9 +98,10 @@ CREATE TABLE `grad_mayor` (
   CONSTRAINT `grad_mayor_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `grad_mayor` */
 
-/*Table structure for table `grad_users` */
+
+# Dump of table grad_users
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `grad_users`;
 
@@ -100,20 +117,30 @@ CREATE TABLE `grad_users` (
   `created_at` int(11) DEFAULT NULL COMMENT 'Fecha Creado',
   `updated_at` int(11) DEFAULT NULL COMMENT 'Fecha Actualizado',
   `auth_key` varchar(128) DEFAULT NULL,
+  `student_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `grad_users` */
+LOCK TABLES `grad_users` WRITE;
+/*!40000 ALTER TABLE `grad_users` DISABLE KEYS */;
 
-insert  into `grad_users`(`id`,`email`,`first_name`,`initial`,`last_name`,`second_last_name`,`password_hash`,`status`,`created_at`,`updated_at`,`auth_key`) values 
-(1,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL),
-(2,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL),
-(3,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL),
-(4,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL),
-(5,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL),
-(6,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL);
+INSERT INTO `grad_users` (`id`, `email`, `first_name`, `initial`, `last_name`, `second_last_name`, `password_hash`, `status`, `created_at`, `updated_at`, `auth_key`, `student_id`)
+VALUES
+	(1,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
+	(2,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
+	(3,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
+	(4,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
+	(5,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
+	(6,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0);
 
+/*!40000 ALTER TABLE `grad_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
