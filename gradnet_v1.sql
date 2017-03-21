d9a27a496a031bdb55f9a889447d7039a4b4891d
robertjdev <r.j.r.eol@gmail.com>
2017-03-21 17:07:11 -0400

    DB Backup

--- gradnet_v1.sql
+++ gradnet_v1.sql
@@ -1,27 +1,74 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.5.5-10.1.16-MariaDB)
-# Database: gradnet_v1
-# Generation Time: 2017-03-17 16:54:59 +0000
-# ************************************************************
+/*
+SQLyog Community v12.3.2 (64 bit)
+MySQL - 10.1.16-MariaDB : Database - gradnet_v1
+*********************************************************************
+*/
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`gradnet_v1` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
+USE `gradnet_v1`;
 
-# Dump of table grad_addresses
-# ------------------------------------------------------------
+/*Table structure for table `auth` */
+
+DROP TABLE IF EXISTS `auth`;
+
+CREATE TABLE `auth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `source` varchar(2056) DEFAULT NULL,
+  `source_id` varchar(2056) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `auth_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `bachelors_degree` */
+
+DROP TABLE IF EXISTS `bachelors_degree`;
+
+CREATE TABLE `bachelors_degree` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dept_id` int(10) DEFAULT NULL,
+  `name_degree` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dept_bachillerato` (`dept_id`),
+  CONSTRAINT `dept_bachillerato` FOREIGN KEY (`dept_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `certification` */
+
+DROP TABLE IF EXISTS `certification`;
+
+CREATE TABLE `certification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `department_id` int(11) DEFAULT NULL,
+  `name_certification` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dept_cert` (`department_id`),
+  CONSTRAINT `dept_cert` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_department` varchar(128) DEFAULT '',
+  `description` varchar(128) DEFAULT NULL,
+  `active` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `grad_addresses` */
 
 DROP TABLE IF EXISTS `grad_addresses`;
 
@@ -40,10 +87,7 @@ CREATE TABLE `grad_addresses` (
   CONSTRAINT `grad_addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table grad_contact_details
-# ------------------------------------------------------------
+/*Table structure for table `grad_contact_details` */
 
 DROP TABLE IF EXISTS `grad_contact_details`;
 
@@ -60,10 +104,7 @@ CREATE TABLE `grad_contact_details` (
   CONSTRAINT `id` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table grad_events_stream
-# ------------------------------------------------------------
+/*Table structure for table `grad_events_stream` */
 
 DROP TABLE IF EXISTS `grad_events_stream`;
 
@@ -77,10 +118,7 @@ CREATE TABLE `grad_events_stream` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table grad_mayor
-# ------------------------------------------------------------
+/*Table structure for table `grad_mayor` */
 
 DROP TABLE IF EXISTS `grad_mayor`;
 
@@ -95,18 +133,18 @@ CREATE TABLE `grad_mayor` (
   `updated_at` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `grad_mayor_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `dept_id` (`dept_id`),
+  CONSTRAINT `grad_mayor_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `grad_mayor_ibfk_2` FOREIGN KEY (`dept_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Dump of table grad_users
-# ------------------------------------------------------------
+/*Table structure for table `grad_users` */
 
 DROP TABLE IF EXISTS `grad_users`;
 
 CREATE TABLE `grad_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) DEFAULT NULL,
   `email` varchar(128) CHARACTER SET latin1 DEFAULT NULL COMMENT 'Email',
   `first_name` varchar(128) CHARACTER SET latin1 DEFAULT NULL COMMENT 'Nombre',
   `initial` varchar(2) CHARACTER SET latin1 DEFAULT NULL COMMENT 'Inicial',
@@ -117,30 +155,28 @@ CREATE TABLE `grad_users` (
   `created_at` int(11) DEFAULT NULL COMMENT 'Fecha Creado',
   `updated_at` int(11) DEFAULT NULL COMMENT 'Fecha Actualizado',
   `auth_key` varchar(128) DEFAULT NULL,
-  `student_id` int(11) DEFAULT NULL,
+  `password_reset_token` varchar(255) DEFAULT NULL,
+  `github` varchar(128) DEFAULT NULL,
+  `username` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `token` */
+
+DROP TABLE IF EXISTS `token`;
+
+CREATE TABLE `token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `code` varchar(32) DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`code`,`type`),
+  CONSTRAINT `token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `grad_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `grad_users` WRITE;
-/*!40000 ALTER TABLE `grad_users` DISABLE KEYS */;
-
-INSERT INTO `grad_users` (`id`, `email`, `first_name`, `initial`, `last_name`, `second_last_name`, `password_hash`, `status`, `created_at`, `updated_at`, `auth_key`, `student_id`)
-VALUES
-	(1,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
-	(2,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
-	(3,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
-	(4,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
-	(5,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0),
-	(6,'roberto.rosa2@upr.edu',NULL,NULL,NULL,NULL,'',10,NULL,NULL,NULL,0);
-
-/*!40000 ALTER TABLE `grad_users` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
